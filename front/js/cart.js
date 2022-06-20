05107b75981994965acf0b2cb18956e6f7e70870
Salama Goumri <salama.gmr.web@gmail.com>
2022-06-20 10:40:08 +0100

    modif

--- front/js/cart.js
+++ front/js/cart.js
@@ -20,11 +20,11 @@ function displayItem(item) {
 
     const cartItemContent = makeCartContent(item)
     article.appendChild(cartItemContent)
-    
+
     displayArticle(article)
     displayTotalPrice()
     displayTotalQuantity()
-    
+
 }
 
 function displayTotalPrice() {
@@ -32,8 +32,8 @@ function displayTotalPrice() {
     const totalPrice = document.querySelector("#totalPrice")
 
     cart.forEach(item => {
-    const priceAdd = item.price * item.quantity
-    total = priceAdd + total
+        const priceAdd = item.price * item.quantity
+        total = priceAdd + total
     })
     totalPrice.textContent = total
 }
@@ -41,11 +41,11 @@ function displayTotalPrice() {
 function displayTotalQuantity() {
     total = 0
     const totalQuantity = document.querySelector("#totalQuantity")
-     cart.forEach(item => {
-         const quantityAdd = item.quantity
-         total = quantityAdd + total
-     })
-     totalQuantity.textContent = total
+    cart.forEach(item => {
+        const quantityAdd = item.quantity
+        total = quantityAdd + total
+    })
+    totalQuantity.textContent = total
 }
 
 function makeCartContent(item) {
@@ -67,17 +67,42 @@ function makeSettings(item) {
     settings.classList.add("cart__item__content__settings")
 
     addQuantityToSettings(settings, item)
-    addDeleteToSettings(settings)
+    addDeleteToSettings(settings, item)
     return settings
 }
 
-function addDeleteToSettings(settings) {
+function addDeleteToSettings(settings, item) {
     const div = document.createElement("div")
     div.classList.add("cart__item__content__settings__delete")
+    div.addEventListener("click", () => deleteItem(item))
     const p = document.createElement("p")
     p.textContent = "Supprimer"
     div.appendChild(p)
     settings.appendChild(div)
+}
+
+function deleteItem(item) {
+    const itemToDelete = cart.find(
+        (product) => product.id === item.id && product.color === item.color
+    )
+    cart.splice(itemToDelete, 1)
+    displayTotalPrice()
+    displayTotalQuantity()
+    deleteData(item)
+    deleteArticleHtml(item)
+}
+
+function deleteData(item) {
+    const key = `${item.id}-${item.color}`
+    localStorage.removeItem(key)
+
+}
+
+function deleteArticleHtml(item) {
+    const deleteArticle = document.querySelector(
+        `article[data-id="${item.id}"][data-color="${item.color}"]`
+    )
+    deleteArticle.remove()
 }
 
 function addQuantityToSettings(settings, item) {
@@ -93,15 +118,15 @@ function addQuantityToSettings(settings, item) {
     input.min = "1"
     input.max = "100"
     input.value = item.quantity
-    input.addEventListener("input",() => updatePriceQuantity(item.id, input.value, item))
-    
+    input.addEventListener("input", () => updatePriceQuantity(item.id, input.value, item))
+
     quantity.appendChild(input)
     settings.appendChild(quantity)
 }
 
 function updatePriceQuantity(id, newValue, item) {
     const updateItem = cart.find((item) => item.id === id)
-    updateItem.quantity = Number(newValue) 
+    updateItem.quantity = Number(newValue)
     item.quantity = updateItem.quantity
     displayTotalQuantity()
     displayTotalPrice()
@@ -116,7 +141,7 @@ function updateDataCart(item) {
 }
 
 function makeDescription(item) {
-    
+
     const description = document.createElement("div")
     description.classList.add("cart__item__content__description")
 
@@ -132,7 +157,7 @@ function makeDescription(item) {
     description.appendChild(h2)
     description.appendChild(p)
     description.appendChild(p2)
-    
+
     return description
 }
 
@@ -159,4 +184,91 @@ function makeImageDiv(item) {
     div.appendChild(image)
 
     return div
+}
+
+
+const orderButton = document.querySelector("#order")
+orderButton.addEventListener("click", (e) => envoiFormulaire(e))
+
+
+function envoiFormulaire(e) {
+    e.preventDefault()
+    if (cart.length === 0) {
+        alert("Votre panier est vide")
+        return
+    }
+
+    if (validationFormulaire()) return
+    if (validationMail()) return
+
+    const body = makePageEnvoi()
+    fetch("http://localhost:3000/api/products/order", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+            
+
+}
+
+function validationFormulaire() {
+    const formulaire = document.querySelector(".cart__order__form")
+    const inputs = formulaire.querySelectorAll("input")
+    inputs.forEach((input) => {
+        if (input.value === "") {
+            alert("Veuillez remplir tout les champs")
+            return true
+        }
+        return false
+    })
+}
+
+function validationMail() {
+    const mail = document.querySelector("#email").value
+    const regex = /^[A-Za-z0-9+_.-]+@(.+)$/
+    if (regex.test(mail) === false) {
+        alert("Addresse mail incorrecte")
+        return true
+    }
+    return false
+}
+
+
+function makePageEnvoi() {
+
+    const formulaire = document.querySelector(".cart__order__form")
+    const firstName = formulaire.elements.firstName.value
+    const lastName = formulaire.elements.lastName.value
+    const address = formulaire.elements.address.value
+    const city = formulaire.elements.city.value
+    const email = formulaire.elements.email.value
+
+    const body = {
+        contact: {
+            firstName: firstName,
+            lastName: lastName,
+            address: address,
+            city: city,
+            email: email
+        },
+        products: ajoutId()
+    }
+    return body
+}
+
+
+function ajoutId() {
+    const qttProducts = localStorage.length
+    const ids = []
+    for (let i = 0; i < qttProducts; i++) {
+        const key = localStorage.key(i)
+        console.log(key)
+        const id = key.split("-")[0]
+        ids.push(id)
+    }
+    return ids
 }
